3175029e434a61d59910e92a94774ae353c1d131
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-13 14:33:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TeskJunior1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day;
+            byte level;
+            int sizeFoot;
+            long customerCount;
+            ulong waitingTimeClient;
+            float playerMoney;
+            double goldCount;
+            bool dogGender;
+            char greeting = '≠';
+            decimal moneyBoss;
+        }
+    }
+}
